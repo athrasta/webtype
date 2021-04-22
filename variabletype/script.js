9bd2fd98bdfa42adac74277d67acbf2a0581d3e9
athrasta <athrasta@risdu.edu>
2021-04-22 00:04:11 -0400

    update

--- variabletype/script.js
+++ variabletype/script.js
@@ -1,52 +1,6 @@
-console.log('script loaded');
-
-// $(document).ready(function(){
-
-// 	console.log('script loaded');
-
-// 	const wghtmax = 100;
-// 	const wdthmax = 150;
 
 
-// $('#default').click(function(event) {
-//     console.log('default clicked');
-//     $("#content").css({
-//         "--wdth": 100,
-//         "--wght": 100,
-//         "--XHGT": 500
-//     });	
-// });
-
-// });
-
-
-
-
-	//mouseposition
-	// $('#intro').mousemove(function(event) {
-
-	// 	console.log(event.pageX, event.pageY);
-
-	// 	let cursorX = event.pageX / $(this).width();
-	// 	let cursorY = 1 - (event.pageY) / $(this).height();
-
-	// 	let settingX = Math.floor(cursorX * wdthmax);
-	// 	let settingY = Math.floor(cursorY * wghtmax);
-
-	// 	// console.log( settingX, settingY)
-
-	// 	$("#bigletter").css({
-	// 		"--wdth": settingX,
-	// 		"--wght": settingY
-	// 	});
-		
-	// });
-
-
-
-// // Function to generate random number 
-// function randomNumber(min, max) { 
-//     return Math.floor(Math.random() * (max - min) + min);
-// }
-
-
+function myFunction() {
+	var element = document.body;
+	element.classList.toggle("dark-mode");
+  }
