1664a4eae12fbfecbc4c5adcb9e39a4485585c8b
athrasta <athrasta@risdu.edu>
2021-05-13 13:05:12 -0400

    slow text

---
+++ prompts/pages/page2.js
@@ -0,0 +1,38 @@
+var typeText = document.querySelector(".typeText")
+var textToBeTyped = "Ask the Objects in your household 'can you help me?' ask them, one by one, and wait, genuinely, as if each were just about to answer you, believe they are about to answer you"
+var index = 0, isAdding = true
+
+function playAnim() {
+  setTimeout(function () {
+    // set the text of typeText to a substring of the text to be typed using index.
+    typeText.innerText = textToBeTyped.slice(0, index)
+    if (isAdding) {
+      // adding text
+      if (index > textToBeTyped.length) {
+        // no more text to add
+        isAdding = false
+        //break: wait 2s before playing again
+        setTimeout(function () {
+          playAnim()
+        }, 2000)
+        return
+      } else {
+        // increment index by 1
+        index++
+      }
+    } else {
+      // removing text
+      if (index === 0) {
+        // no more text to remove
+        isAdding = true
+      } else {
+        // decrement index by 1
+        index--
+      }
+    }
+    // call itself
+    playAnim()
+  }, 720)
+}
+// start animation
+playAnim()
